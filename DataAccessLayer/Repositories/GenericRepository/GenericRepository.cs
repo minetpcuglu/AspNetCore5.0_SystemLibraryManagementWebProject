2e845e1ec7b7609902d1070182bf11eaf48dbb32
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-11-10 11:29:19 +0300

    10.11 /DAL - Added Repository Pattern Integration

---
+++ DataAccessLayer/Repositories/GenericRepository/GenericRepository.cs
@@ -0,0 +1,55 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories.GenericRepository
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+
+        public void Delete(T t)
+        {
+            using var c = new Context();
+            c.Remove(t);
+            c.SaveChanges();
+           
+        }
+
+        public List<T> GetAll()
+        {
+            using var c = new Context();
+            return c.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            using var c = new Context();
+            return c.Set<T>().Find(id);
+        }
+
+        public List<T> GetListAll(Expression<Func<T, bool>> filter)
+        {
+            using var c = new Context();
+            return c.Set<T>().Where(filter).ToList();
+        }
+
+        public void insert(T t)
+        {
+            using var c = new Context();
+            c.Add(t);
+            c.SaveChanges();
+        }
+
+        public void Update(T t)
+        {
+            using var c = new Context();
+            c.Update(t);
+            c.SaveChanges();
+        }
+    }
+}
