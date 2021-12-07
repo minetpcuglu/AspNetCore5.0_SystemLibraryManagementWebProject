93bb89c8dafa65bed3463fb60a0889ccb69fb214
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-12-08 01:34:00 +0300

    08.12 / Writer Controller Thema List

---
+++ AspNetCore5.0_SystemLibraryManagementWebProject/Controllers/WriterController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore5._0_SystemLibraryManagementWebProject.Controllers
+{
+    public class WriterController : Controller
+    {
+        WriterManager writerManager = new WriterManager(new EfWriterRepository());
+        public IActionResult Index()
+        {
+            var value = writerManager.GetList();
+            return View();
+        }
+    }
+}
