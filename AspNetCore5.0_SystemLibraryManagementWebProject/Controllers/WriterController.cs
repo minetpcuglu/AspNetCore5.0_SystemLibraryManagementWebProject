067d4a53775666971b6e7167706f79af48957a95
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-12-09 02:49:10 +0300

    09.12/Writer Image File Added

--- AspNetCore5.0_SystemLibraryManagementWebProject/Controllers/WriterController.cs
+++ AspNetCore5.0_SystemLibraryManagementWebProject/Controllers/WriterController.cs
@@ -3,9 +3,11 @@ using BusinessLayer.ValidationRules.FluentValidation;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using FluentValidation.Results;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -62,5 +64,54 @@ namespace AspNetCore5._0_SystemLibraryManagementWebProject.Controllers
 
       
         }
+
+        [HttpGet]
+        public IActionResult WriterEditProfile(int id)
+        {
+            var writervalue = writerManager.GetById(id);
+            return View(writervalue);
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> WriterEditProfile(Writer writer, IFormFile file)
+        {
+            ValidationResult result =writerRules.Validate(writer);
+            if (result.IsValid)
+            {
+                if (file != null)
+                {
+                    var extension = Path.GetExtension(file.FileName); //uzantiya ulasmak //.jpg .png
+                    var randomFileName = string.Format($"{Guid.NewGuid()}{extension}");  //random bir sayı ile resim dosyaları birbirine çakışmaması
+                    var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\img", randomFileName);
+                    writer.Image = randomFileName;
+
+                    using (var stream = new FileStream(path, FileMode.Create))  //using içinde olması isimiz bittiginde otamatşk silinecek olması.
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                }
+
+                writerManager.Update(writer);
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.PropertyName, error.ErrorMessage);
+                }
+            }
+            return View();
+
+
+
+
+
+
+
+        }
     }
 }
+
