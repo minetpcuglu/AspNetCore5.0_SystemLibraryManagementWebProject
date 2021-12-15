f134f690fb146ff7e7eb88fbc240e9c1b4df40eb
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-12-15 15:35:03 +0300

    15.12 / Employee Validation Rule

--- AspNetCore5.0_SystemLibraryManagementWebProject/Controllers/EmployeeController.cs
+++ AspNetCore5.0_SystemLibraryManagementWebProject/Controllers/EmployeeController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules.FluentValidation;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -13,6 +15,7 @@ namespace AspNetCore5._0_SystemLibraryManagementWebProject.Controllers
     public class EmployeeController : Controller
     {
         EmployeeManager employeeManager = new EmployeeManager(new EfEmployeeRepository());
+        EmployeeValidation employeeRules = new EmployeeValidation();
         public IActionResult Index(int page =1)
         {
             var value = employeeManager.GetList();
@@ -28,8 +31,20 @@ namespace AspNetCore5._0_SystemLibraryManagementWebProject.Controllers
         [HttpPost]
         public IActionResult AddEmployee(Employee employee)
         {
-            employeeManager.Add(employee);
-            return RedirectToAction("Index");
+            ValidationResult result = employeeRules.Validate(employee);
+            if (result.IsValid)
+            {
+                employeeManager.Add(employee);
+                return RedirectToAction("Index");
+            }
+            else if (!result.IsValid)
+            {
+                foreach (var rule in result.Errors)
+                {
+                    ModelState.AddModelError(rule.PropertyName, rule.ErrorMessage);
+                }
+            }
+            return View();
         }
     }
 }
---
+++ BusinessLayer/ValidationRules/FluentValidation/EmployeeValidation.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules.FluentValidation
+{
+   public class EmployeeValidation:AbstractValidator<Employee>
+    {
+        public EmployeeValidation()
+        {
+            RuleFor(x => x.NameSurname).NotEmpty().WithMessage("Personel Adı Soyadı Boş Geçilemez");
+            RuleFor(x => x.NameSurname).MinimumLength(5).MaximumLength(35).WithMessage("Personel Adı Soyadı min 5 max 35  karakter içermeli");
+        }
+    }
+}
