8341335c63cb9733fd0f205448a668b515c05d2d
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-12-18 15:58:05 +0300

    18/12 UserGetList

---
+++ AspNetCore5.0_SystemLibraryManagementWebProject/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using X.PagedList;
+
+namespace AspNetCore5._0_SystemLibraryManagementWebProject.Controllers
+{
+    public class UserController : Controller
+    {
+        UserManager userManager = new UserManager(new EfUserRepository());
+        public IActionResult Index(int page=1)
+        {
+            var value = userManager.GetList();
+            return View(value.ToPagedList(page, 10));
+        }
+    }
+}
