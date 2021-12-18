9ef0bdfc918e4086e959eb541952e03d4c13fc7b
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-12-18 16:52:13 +0300

    18.12 / AddUser -1

--- AspNetCore5.0_SystemLibraryManagementWebProject/Controllers/UserController.cs
+++ AspNetCore5.0_SystemLibraryManagementWebProject/Controllers/UserController.cs
@@ -1,8 +1,13 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules.FluentValidation;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using X.PagedList;
@@ -12,10 +17,57 @@ namespace AspNetCore5._0_SystemLibraryManagementWebProject.Controllers
     public class UserController : Controller
     {
         UserManager userManager = new UserManager(new EfUserRepository());
+        UserValidator userRules = new UserValidator();
         public IActionResult Index(int page=1)
         {
             var value = userManager.GetList();
             return View(value.ToPagedList(page, 10));
+        }
+
+        [HttpGet]
+        public IActionResult AddUser()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddUser(User user, IFormFile file)
+        {
+            ValidationResult result = userRules.Validate(user);
+
+            if (result.IsValid)
+            {
+                if (file != null)
+                {
+                    var extension = Path.GetExtension(file.FileName); //uzantiya ulasmak //.jpg .png
+                    var randomFileName = string.Format($"{Guid.NewGuid()}{extension}");  //random bir sayı ile resim dosyaları birbirine çakışmaması
+                    var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\img", randomFileName);
+                    user.Image = randomFileName;
+
+                    using (var stream = new FileStream(path, FileMode.Create))  //using içinde olması isimiz bittiginde otamatşk silinecek olması.
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                }
+
+
+                userManager.Add(user);
+                return RedirectToAction("Index");
+            }
+            else if (!result.IsValid)
+            {
+                foreach (var rule in result.Errors)
+                {
+                    ModelState.AddModelError(rule.PropertyName, rule.ErrorMessage);
+                }
+
+
+            }
+
+            return View();
+
+
         }
     }
 }
---
+++ BusinessLayer/ValidationRules/FluentValidation/UserValidator.cs
@@ -0,0 +1,45 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules.FluentValidation
+{
+   public class UserValidator:AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Kullanıcı adı boş geçilemez");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Kullanıcı Soyadı boş geçilemez");
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("Kullanıcı boş geçilemez");
+            RuleFor(x => x.Name).MaximumLength(50).WithMessage(" Adı maks 50 karakter olmalı");
+            RuleFor(x => x.Name).MinimumLength(3).WithMessage(" Adı min 3 karakter olmalı");
+            RuleFor(x => x.Surname).MaximumLength(50).WithMessage(" Kullanıcı Soyadı maks 50 karakter olmalı");
+            RuleFor(x => x.Surname).MinimumLength(3).WithMessage("Kullanıcı Soyadı min 3 karakter olmalı");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Kullanıcı soyadı boş geçilemez");
+            RuleFor(x => x.Telephone).NotEmpty().WithMessage("Telefon boş geçilemez");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Kullanıcı şifre boş geçilemez");
+            RuleFor(x => x.Password).Must(IsPasswordValidRules).WithMessage("Parolanızda en az bir küçük harf bir büyük harf ve rakamdan oluşmalıdır.");
+            RuleFor(x => x.Email).NotEmpty().EmailAddress().WithMessage("Kullanıcı Mail boş geçilemez / E-mail formatında olmalı");
+            RuleFor(x => x.Surname).MaximumLength(50).WithMessage("Yazar soyadı maks 50 karakter olmalı");
+            RuleFor(x => x.Surname).MinimumLength(2).WithMessage("Yazar soyadı min 2 karakter olmalı");
+      
+        }
+        private bool IsPasswordValidRules(string arg)
+        {
+            try
+            {
+                Regex userPassword = new Regex(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[0-9])[A-Za-z\d]");
+                return userPassword.IsMatch(arg);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
