13e648e2d67852f4ccd20411890b8b121260638e
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-12-19 23:03:03 +0300

    19.12/ Loan -2

--- AspNetCore5.0_SystemLibraryManagementWebProject/Controllers/LoanedController.cs
+++ AspNetCore5.0_SystemLibraryManagementWebProject/Controllers/LoanedController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,13 +11,22 @@ namespace AspNetCore5._0_SystemLibraryManagementWebProject.Controllers
 {
     public class LoanedController : Controller  //takeonloan ödünc alma 
     {
+        MovementManager movementManager = new MovementManager(new EfMovementRepository());
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
         public IActionResult Loan() //Ödünç ver 
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Loan(Movement movement) //Ödünç ver 
+        {
+           movementManager.Add(movement);
             return View();
         }
     }
