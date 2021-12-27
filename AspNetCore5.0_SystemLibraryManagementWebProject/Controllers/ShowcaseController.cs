c21f5847bce79ac0626bc612dfa2e58dd21ececa
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-12-28 01:19:48 +0300

    28.12 / View Components BookList

--- AspNetCore5.0_SystemLibraryManagementWebProject/Controllers/ShowcaseController.cs
+++ AspNetCore5.0_SystemLibraryManagementWebProject/Controllers/ShowcaseController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,11 @@ namespace AspNetCore5._0_SystemLibraryManagementWebProject.Controllers
 {
     public class ShowcaseController : Controller
     {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
         public IActionResult Index()
         {
-            return View();
+            var value = categoryManager.GetList();
+            return View(value);
         }
     }
 }
---
+++ AspNetCore5.0_SystemLibraryManagementWebProject/ViewComponents/Book/ShowcaseBookList.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore5._0_SystemLibraryManagementWebProject.ViewComponents.Book
+{
+    public class ShowcaseBookList : ViewComponent
+    {
+        BookManager bookManager = new BookManager(new EfBookRepository());
+        public IViewComponentResult Invoke()
+        {
+
+            var Value = bookManager.GetList();
+            return View(Value);
+        }
+    }
+}
