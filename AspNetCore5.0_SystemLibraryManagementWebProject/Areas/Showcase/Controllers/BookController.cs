0a5cbd4c535d01331fa36530075950e20426f5db
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-12-29 01:55:25 +0300

    29.12 / Showcase Area

---
+++ AspNetCore5.0_SystemLibraryManagementWebProject/Areas/Showcase/Controllers/BookController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore5._0_SystemLibraryManagementWebProject.Areas.Showcase.Controllers
+{
+    [Area("Showcase")]
+    public class BookController : Controller
+    {
+        BookManager bookManager = new BookManager(new EfBookRepository());
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetlistBookId(int id)
+        {
+            var value = bookManager.GetListWithCategoryBookId(id);
+            return View(value);
+        }
+    }
+}
--- AspNetCore5.0_SystemLibraryManagementWebProject/Startup.cs
+++ AspNetCore5.0_SystemLibraryManagementWebProject/Startup.cs
@@ -48,9 +48,17 @@ namespace AspNetCore5._0_SystemLibraryManagementWebProject
 
             app.UseEndpoints(endpoints =>
             {
+               endpoints.MapControllerRoute(
+               name: "areas",
+               pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+
+
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
             });
         }
     }
