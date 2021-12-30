a883b598140e40366f61dd7ecf69b1ec9fa6b6c3
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-12-30 03:59:21 +0300

    30.12/ WriterArea PopulerBook VC's

---
+++ AspNetCore5.0_SystemLibraryManagementWebProject/Areas/Showcase/ViewComponents/Writer/AreaPopulerBookWithWriter.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore5._0_SystemLibraryManagementWebProject.Areas.Showcase.ViewComponents.Writer
+{
+    public class AreaPopulerBookWithWriter:ViewComponent
+    {
+        BookManager bookManager = new BookManager(new EfBookRepository());
+        public IViewComponentResult Invoke(int id)
+        {
+
+            var Value = bookManager.GetListWithCategoryByWriterId(id);
+            return View(Value);
+        }
+    }
+  
+}
