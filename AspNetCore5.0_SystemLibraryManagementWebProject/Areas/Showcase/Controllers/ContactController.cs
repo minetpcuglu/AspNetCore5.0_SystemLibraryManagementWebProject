380f8b18ec3eaf2a9177024354db071c46404139
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-12-31 02:01:51 +0300

    31.12 /Contact Page

--- AspNetCore5.0_SystemLibraryManagementWebProject/Areas/Showcase/Controllers/ContactController.cs
+++ AspNetCore5.0_SystemLibraryManagementWebProject/Areas/Showcase/Controllers/ContactController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,12 +9,27 @@ using System.Threading.Tasks;
 
 namespace AspNetCore5._0_SystemLibraryManagementWebProject.Areas.Showcase.Controllers
 {
+    [Area("Showcase")]
     public class ContactController : Controller
     {
-        
+        ContactManager contactManager = new ContactManager(new EfContactRepository());
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddContact()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddContact(Contact contact)
+        {
+            contactManager.Add(contact);
+            return RedirectToAction("AddContact");
         }
     }
 }
