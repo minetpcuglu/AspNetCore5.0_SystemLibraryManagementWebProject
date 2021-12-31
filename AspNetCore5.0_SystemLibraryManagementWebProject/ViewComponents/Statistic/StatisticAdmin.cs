4227dd5e4918a6306b88f50983e6e91a6dee19d5
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-12-31 17:16:54 +0300

    31.12 / WriterStatistic

---
+++ AspNetCore5.0_SystemLibraryManagementWebProject/ViewComponents/Statistic/StatisticAdmin.cs
@@ -0,0 +1,21 @@
+﻿using DataAccessLayer.Concrete.Context;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore5._0_SystemLibraryManagementWebProject.ViewComponents.Statistic
+{
+    public class StatisticAdmin : ViewComponent
+    {
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.adminName = c.Writers.Where(x => x.WriterId == 1).Select(y => y.Name).FirstOrDefault();
+            ViewBag.adminImage = c.Writers.Where(x => x.WriterId == 1).Select(y => y.Image).FirstOrDefault();
+            ViewBag.adminDescription = c.Writers.Where(x => x.WriterId == 1).Select(y => y.Description).FirstOrDefault();
+            return View();
+        }
+    }
+}
---
+++ AspNetCore5.0_SystemLibraryManagementWebProject/ViewComponents/Statistic/StatisticLastBook.cs
@@ -0,0 +1,21 @@
+﻿using DataAccessLayer.Concrete.Context;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore5._0_SystemLibraryManagementWebProject.ViewComponents.Statistic
+{
+    public class StatisticLastBook : ViewComponent
+    {
+        Context c = new Context(); //Toplam Yeni mesaj sayısı
+        public IViewComponentResult Invoke()
+        {
+
+            ViewBag.lastBook = c.Books.OrderByDescending(x => x.BookId).Select(x => x.BookName).Take(1).FirstOrDefault();//son eklenen kitap
+            ViewBag.totelUser = c.Users.Count(); //toplam Kullanıcı sayısı
+            return View();
+        }
+    }
+}
