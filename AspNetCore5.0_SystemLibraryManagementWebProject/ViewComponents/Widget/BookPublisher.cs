69386ef6c216fd4319a1a6773c50812aa7736430
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-01-08 22:14:53 +0300

    08.01 / Widget Ling Finish

---
+++ AspNetCore5.0_SystemLibraryManagementWebProject/ViewComponents/Widget/BookPublisher.cs
@@ -0,0 +1,22 @@
+﻿using DataAccessLayer.Concrete.Context;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore5._0_SystemLibraryManagementWebProject.ViewComponents.Widget
+{
+    public class BookPublisher : ViewComponent
+    {
+        Context c = new Context(); //Toplam Yeni mesaj sayısı
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.BookPublish = c.Books.GroupBy(x=>x.Publisher).OrderByDescending(y=>y.Count()).Select(z=> new {z.Key }).FirstOrDefault();//son eklenen kitap
+
+
+
+            return View();
+        }
+    }
+}
