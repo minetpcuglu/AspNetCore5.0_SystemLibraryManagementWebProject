ce58ad750c10d421f9c69a766121e5fc83ad3664
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-01-08 21:50:39 +0300

    08.01 / VC's -4

---
+++ AspNetCore5.0_SystemLibraryManagementWebProject/ViewComponents/Widget/ContactCount.cs
@@ -0,0 +1,24 @@
+﻿using DataAccessLayer.Concrete.Context;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore5._0_SystemLibraryManagementWebProject.ViewComponents.Widget
+{
+    public class ContactCount : ViewComponent
+    {
+
+        Context c = new Context(); //Toplam Yeni mesaj sayısı
+        public IViewComponentResult Invoke()
+        {
+
+            ViewBag.ContactCount = c.Contacts.Count();
+         
+
+            return View();
+        }
+
+    }
+}
---
+++ AspNetCore5.0_SystemLibraryManagementWebProject/ViewComponents/Widget/WriterTheMostBook.cs
@@ -0,0 +1,24 @@
+﻿using DataAccessLayer.Concrete.Context;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore5._0_SystemLibraryManagementWebProject.ViewComponents.Widget
+{
+    public class WriterTheMostBook : ViewComponent
+    {
+
+        Context c = new Context(); //Toplam Yeni mesaj sayısı
+        public IViewComponentResult Invoke()
+        {
+
+            ViewBag.WriterName= c.Writers.OrderByDescending(x => x.WriterId).Select(x => x.Name + " " + x.Surname).Take(1).FirstOrDefault();
+
+
+            return View();
+        }
+
+    }
+}
