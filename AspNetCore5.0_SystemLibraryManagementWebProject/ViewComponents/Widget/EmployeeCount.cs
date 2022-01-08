f3c46052ef65b2f5c9dde814c7a4d91228fcc8a1
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-01-08 21:06:15 +0300

    08.01 / VC's -3

---
+++ AspNetCore5.0_SystemLibraryManagementWebProject/ViewComponents/Widget/EmployeeCount.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessLayer.Concrete.Context;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore5._0_SystemLibraryManagementWebProject.ViewComponents.Widget
+{
+    public class EmployeeCount : ViewComponent
+    {
+        Context c = new Context(); //Toplam kullanıcı sayısı
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.EmployeeCount = c.Employees.Count();
+
+            return View();
+        }
+    }
+}
