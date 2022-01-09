fe8d01bc50cadc2889744b9e7bcc8bf37a5fcbad
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-01-09 14:12:20 +0300

    09.01 / Login - Register -1

---
+++ DataAccessLayer/Encokkitabıolanyazar.sql
@@ -0,0 +1,8 @@
+--create Procedure EnCokKitabýOlanYazar
+
+--As
+--Select Top 1 Writers.Name + Writers.Surname  From Books
+--Inner Join Writers On Writers.WriterId = Books.WriterId
+--Group By Writers.Name + Writers.Surname order by Count(*) Desc
+
+execute EnCokKitabýOlanYazar
