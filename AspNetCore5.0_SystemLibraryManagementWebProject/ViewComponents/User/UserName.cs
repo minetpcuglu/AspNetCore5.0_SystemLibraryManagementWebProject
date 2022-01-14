e629a7d80b940e476a5958eb5f9fd6282c87a10f
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-01-15 02:41:39 +0300

    15.01 / Revize

---
+++ AspNetCore5.0_SystemLibraryManagementWebProject/ViewComponents/User/UserName.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.Context;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore5._0_SystemLibraryManagementWebProject.ViewComponents.User
+{
+    public class UserName: ViewComponent
+    {
+        UserManager userManager = new UserManager(new EfUserRepository());
+        public IViewComponentResult Invoke()
+        {
+            Context c = new Context();
+            var userName = User.Identity.Name;
+            //sisteme otantike olan kullanıcının bilgilerinin gelmesi
+            var userId = c.Users.Where(x => x.Name + x.Surname == userName).Select(y => y.UserId).FirstOrDefault();
+            var uservalue = userManager.GetById(userId);
+            return View(uservalue);
+        }
+    }
+}
