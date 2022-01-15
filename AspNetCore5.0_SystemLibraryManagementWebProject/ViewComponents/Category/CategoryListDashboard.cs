493293669cc91f80d17a55ca9e6dfc193ce80257
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-01-15 17:11:21 +0300

    15.01 / Dashboard Completed VC's Added

---
+++ AspNetCore5.0_SystemLibraryManagementWebProject/ViewComponents/Category/CategoryListDashboard.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore5._0_SystemLibraryManagementWebProject.ViewComponents.Category
+{
+    public class CategoryListDashboard : ViewComponent
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
+        public IViewComponentResult Invoke()
+        {
+            var Value = categoryManager.GetList();
+            return View(Value);
+        }
+    }
+}
