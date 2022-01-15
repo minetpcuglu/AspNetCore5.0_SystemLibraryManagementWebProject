b94f613fef7432624017e73986e697065dc3dcfa
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-01-15 16:33:33 +0300

    15.01 / Dashboard Update

--- AspNetCore5.0_SystemLibraryManagementWebProject/Controllers/DashboardController.cs
+++ AspNetCore5.0_SystemLibraryManagementWebProject/Controllers/DashboardController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +10,16 @@ namespace AspNetCore5._0_SystemLibraryManagementWebProject.Controllers
 {
     public class DashboardController : Controller
     {
+        BookManager bookManager = new BookManager(new EfBookRepository());
+       CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
+        MovementManager movementManager = new MovementManager(new EfMovementRepository());
+       
+       
         public IActionResult Index()
         {
+            ViewBag.CountBookTotel = bookManager.GetList().Count();
+            ViewBag.CountCategoryTotel = categoryManager.GetList().Count();
+            ViewBag.CountLoanBookTotel = movementManager.GetList().Where(x => x.IslemDurum == true).Count();
             return View();
         }
     }
---
+++ AspNetCore5.0_SystemLibraryManagementWebProject/ViewComponents/Book/BookListDashboard.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.Context;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using X.PagedList;
+
+namespace AspNetCore5._0_SystemLibraryManagementWebProject.ViewComponents.Book
+{
+    public class BookListDashboard:ViewComponent
+    {
+        BookManager bookManager = new BookManager(new EfBookRepository());
+        Context c = new Context();
+
+        public IViewComponentResult Invoke()
+        {
+            //var Value= c.Books.OrderByDescending(x => x.BookId).Select(x => x.BookName).Take(4).FirstOrDefault();//son eklenen blog
+            var Value = bookManager.GetListWithCategory().Take(7).ToList();//son eklenen kitaplar
+            return View(Value);
+        }
+    }
+}
