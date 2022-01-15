53e9e75afbf7e9ab0f93ce9af641a0873344fdb4
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-01-16 01:36:50 +0300

    16.01 / Message

---
+++ BusinessLayer/Concrete/MessageManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+   public class MessageManager:IMessageService
+    {
+        IMessageDal _messageDal;
+
+        public MessageManager(IMessageDal messageDal)
+        {
+            _messageDal = messageDal;
+        }
+
+        public void Add(Message t)
+        {
+            _messageDal.insert(t);
+        }
+
+        public void Delete(Message t)
+        {
+            _messageDal.Delete(t);
+        }
+
+        public Message GetById(int id)
+        {
+            return _messageDal.GetById(id);
+        }
+
+        public List<Message> GetList()
+        {
+           return _messageDal.GetAll();
+        }
+
+        public void Update(Message t)
+        {
+            _messageDal.Update(t);
+        }
+    }
+}
---
+++ DataAccessLayer/EntityFramework/EfMessageRepository.cs
@@ -0,0 +1,14 @@
+﻿using DataAccessLayer.Repositories.GenericRepository;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.EntityFramework
+{
+   public class EfMessageRepository : GenericRepository<Message>, IMessageDal
+    {
+    }
+}
