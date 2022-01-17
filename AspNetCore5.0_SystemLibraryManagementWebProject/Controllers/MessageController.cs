cde427f9ec320a93d08a65ef054e6ba660e227e2
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-01-17 23:28:43 +0300

    17.01 / Message Inbox

---
+++ AspNetCore5.0_SystemLibraryManagementWebProject/Controllers/MessageController.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore5._0_SystemLibraryManagementWebProject.Controllers
+{
+    [AllowAnonymous]
+    public class MessageController : Controller
+    {
+        MessageManager messageManager = new MessageManager(new EfMessageRepository());
+        public IActionResult Inbox() /*Gelen kutusu*/
+        {
+
+            int id = 3;
+            var value = messageManager.GetInboxListByUser(id);
+            return View(value);
+            
+        }
+    }
+}
--- DataAccessLayer/EntityFramework/EfMessageRepository.cs
+++ DataAccessLayer/EntityFramework/EfMessageRepository.cs
@@ -13,7 +13,7 @@ namespace DataAccessLayer.EntityFramework
 {
    public class EfMessageRepository : GenericRepository<Message>, IMessageDal
     {
-        public List<Message> GetListWithMessageByUser(int id) //kategoriye göre yazar getirme
+        public List<Message> GetListWithMessageByUser(int id) //kullanıya göre message getirme 
         {
             using (var c = new Context())
             {
