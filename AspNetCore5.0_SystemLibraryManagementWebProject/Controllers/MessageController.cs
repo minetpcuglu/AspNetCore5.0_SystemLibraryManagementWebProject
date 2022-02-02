31ae5a98bee2cafee015000146991a2b535f225f
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-02-03 01:57:50 +0300

    03.02.2022

--- AspNetCore5.0_SystemLibraryManagementWebProject/Controllers/MessageController.cs
+++ AspNetCore5.0_SystemLibraryManagementWebProject/Controllers/MessageController.cs
@@ -1,5 +1,7 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.Context;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -15,11 +17,27 @@ namespace AspNetCore5._0_SystemLibraryManagementWebProject.Controllers
         MessageManager messageManager = new MessageManager(new EfMessageRepository());
         public IActionResult Inbox() /*Gelen kutusu*/
         {
+            //var username = User.Identity.Name;  //sisteme giren kullanıcı adı soyadı
+            //ViewBag.value = username;
+            //Context c = new Context();
+            //var userName = c.Users.Where(x => x.UserName == username).Select(y => y.Name + " " + y.Surname).FirstOrDefault();
+            //ViewBag.value2 = userName;
+
+            //var userImage = c.Users.Where(x => x.UserName == username).Select(y => y.Image).FirstOrDefault();
+            //ViewBag.Image = userImage;
+            //return View();
+
+            // Context c = new Context();
+            //var userName = User.Identity.Name;
+            ////sisteme otantike olan kullanıcının bilgilerinin gelmesi
+            //var userId = c.Messages.Where(x => x.ReceiverUser.UserName == userName).Select(y => y.MessageId).FirstOrDefault(); ** hata burada 
+            //var uservalue = messageManager.GetInboxListByUser(userId);
+            ////return View(uservalue);
 
             int id = 3;
             var value = messageManager.GetInboxListByUser(id);
             return View(value);
-            
+
         }
         public IActionResult MessageDetails(int id)
         {
@@ -27,6 +45,26 @@ namespace AspNetCore5._0_SystemLibraryManagementWebProject.Controllers
 
             var value = messageManager.GetById(id);
             return View(value);
+        }
+
+        [HttpGet]
+        public IActionResult NewMessage()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NewMessage(Message message)
+        {
+            //var mail = (string).Session["UserName"].ToString();
+            //message.ReceiverUser = mail.tostring();
+
+            message.MessageDate.ToShortDateString();
+            message.MessageStatus = true;
+            messageManager.Add(message);
+            
+            return RedirectToAction("Inbox");
         }
     }
 }
