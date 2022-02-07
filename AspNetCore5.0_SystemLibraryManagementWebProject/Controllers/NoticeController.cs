ce62aafdb3a66e0399a34879bc14a4930e6b0ef5
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-02-07 19:30:16 +0300

    07.02 / Notice CRUD

--- AspNetCore5.0_SystemLibraryManagementWebProject/Controllers/NoticeController.cs
+++ AspNetCore5.0_SystemLibraryManagementWebProject/Controllers/NoticeController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -16,5 +17,48 @@ namespace AspNetCore5._0_SystemLibraryManagementWebProject.Controllers
             var value = noticeManager.GetList();
             return View(value);
         }
+
+
+
+        [HttpGet]
+        public IActionResult AddNotice()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddNotice(Notice notice)
+        {
+          
+                noticeManager.Add(notice);
+                return RedirectToAction("Index");
+           
+        }
+
+
+        public IActionResult DeleteNotice(int id)
+        {
+            var value = noticeManager.GetById(id);
+            noticeManager.Delete(value);
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public IActionResult NoticeUpdate(int id)
+        {
+            var value = noticeManager.GetById(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult NoticeUpdate(Notice notice)
+        {
+           
+                noticeManager.Update(notice);
+                return RedirectToAction("Index");
+           
+        }
+
     }
 }
