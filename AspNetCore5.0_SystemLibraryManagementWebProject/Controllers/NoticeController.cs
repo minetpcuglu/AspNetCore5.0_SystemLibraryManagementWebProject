46c7064bffc0e1faccd38746d8a00a1d157cc118
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-02-07 19:19:07 +0300

    07.02 / Notice Get List

---
+++ AspNetCore5.0_SystemLibraryManagementWebProject/Controllers/NoticeController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore5._0_SystemLibraryManagementWebProject.Controllers
+{
+    public class NoticeController : Controller
+    {
+        NoticeManager noticeManager = new NoticeManager(new EfNoticeRepository());
+        public IActionResult Index()
+        {
+            var value = noticeManager.GetList();
+            return View(value);
+        }
+    }
+}
