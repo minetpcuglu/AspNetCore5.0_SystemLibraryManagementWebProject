5fe7c97d63c537d34185907dc7c5540d465963e1
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-02-07 18:02:53 +0300

    07.02 / DropDownList

--- AspNetCore5.0_SystemLibraryManagementWebProject/Controllers/LoanedController.cs
+++ AspNetCore5.0_SystemLibraryManagementWebProject/Controllers/LoanedController.cs
@@ -3,6 +3,7 @@ using DataAccessLayer.Concrete.Context;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,22 +16,27 @@ namespace AspNetCore5._0_SystemLibraryManagementWebProject.Controllers
 
         MovementManager movementManager = new MovementManager(new EfMovementRepository());
         CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
+        UserManager userManager = new UserManager(new EfUserRepository());
+        BookManager bookManager = new BookManager(new EfBookRepository());
+        EmployeeManager employeeManager = new EmployeeManager(new EfEmployeeRepository());
         Context c = new Context();
-       
+
         public IActionResult Index()
         {
             return View();
         }
 
-      
+
         [HttpGet]
         public IActionResult LoanBookList() //Ödünç kitap Listesi
         {
-            var value = movementManager.GetListWithBook().Where(x => x.IslemDurum == false).ToList() ;
-            return View( value);
+          
+
+            var value = movementManager.GetListWithBook().Where(x => x.IslemDurum == false).ToList();
+            return View(value);
         }
 
-     
+
 
         //[HttpPost]
         //public IActionResult LoanBookList([Bind(Prefix =) //Ödünç kitap Listesi
@@ -42,26 +48,56 @@ namespace AspNetCore5._0_SystemLibraryManagementWebProject.Controllers
         [HttpGet]
         public IActionResult Loan() //Ödünç ver 
         {
-         
+            List<SelectListItem> userValue = (from x in userManager.GetList()
+                                              select new SelectListItem
+                                              {
+                                                  Text = x.Name + " " + x.Surname,
+                                                  Value = x.UserId.ToString()
+                                              }).ToList();
+
+            ViewBag.value = userValue;
+
+            List<SelectListItem> employeeValue = (from x in employeeManager.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.NameSurname,
+                                                      //Text = x.e + " " + x.Surname,
+
+                                                      Value = x.EmployeeId.ToString()
+                                                  }).ToList();
+
+            ViewBag.value2 = employeeValue;
+
+            List<SelectListItem> bookValue = (from x in bookManager.GetList()
+                                              select new SelectListItem
+                                              {
+                                                  Text = x.BookName,
+
+
+                                                  Value = x.BookId.ToString()
+                                              }).ToList();
+
+            ViewBag.value3 = bookValue;
             return View();
+        
         }
 
         [HttpPost]
         public IActionResult Loan(Movement movement) //Ödünç ver 
         {
-           movementManager.Add(movement);
+            movementManager.Add(movement);
             return RedirectToAction("LoanBookList");
         }
 
         //[HttpGet]
         //public IActionResult TakeOnLoan() //ödünç iade
         //{
-            
+
         //    return View(/*Tuple.Create<Movement,Book,Employee,User>(new Movement(),new Book(),new Employee(),new User())*/);
         //}
-         /*,[Bind(Prefix ="kullanici")]User Model1, [Bind(Prefix = "personel")] Employee Model2, [Bind(Prefix = "kitap")]Book Model3, [Bind(Prefix = "hareket")] Movement Model4*/
-       
-        
+        /*,[Bind(Prefix ="kullanici")]User Model1, [Bind(Prefix = "personel")] Employee Model2, [Bind(Prefix = "kitap")]Book Model3, [Bind(Prefix = "hareket")] Movement Model4*/
+
+
         public IActionResult TakeOnLoan(int id) //ödünç iade
         {
             var value = movementManager.GetById(id);
@@ -69,12 +105,12 @@ namespace AspNetCore5._0_SystemLibraryManagementWebProject.Controllers
             DateTime date2 = Convert.ToDateTime(DateTime.Now.ToShortDateString());
             TimeSpan date3 = date2 - date;
             ViewBag.dgr = date3.TotalDays;
-         
-         
-            return View("TakeOnLoan",value);
+
+
+            return View("TakeOnLoan", value);
         }
 
-    
+
         public IActionResult TakeOnLoanUpdate(Movement movement) //Ödünç ver 
         {
             var value = c.Movements.Find(movement.MovementId);
@@ -84,6 +120,6 @@ namespace AspNetCore5._0_SystemLibraryManagementWebProject.Controllers
 
             return RedirectToAction("LoanBookList");
         }
-       
+
     }
 }
